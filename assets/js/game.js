8b51301bad2e7d3900ab95f508ffd2bc67cd774c
unknown <mcsalazar122@gmail.com>
2022-05-10 17:03:52 -0500

    added to javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+//fight();
+
+console.log(playerName);
+
+console.log("Our robot's name is " + playerName);
+
